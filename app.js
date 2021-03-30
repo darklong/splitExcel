1cb4b5a8576e6f25b4ad6650982a75609197acef
darklong <dvvlong1990@gmail.com>
2021-03-30 23:13:01 +0700

    0.1.0

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { flatMap } = require('lodash');
 const _ = require('lodash')
 const readXlsxFile = require("read-excel-file/node");
 
@@ -23,24 +24,48 @@ readXlsxFile(`./IN/Book1.xlsx`).then((rows) => {
       }
   
     let dataSort = _.sortBy(arrObj, 'MAKH') 
-    
+    // let dataSortTemp = _.sortBy(arrObj, 'MAKH')
     let filenum = 1
-    let flat = 1
+    let flag = 1
     
     for(let i = 0; i < dataSort.length; i++)
     {
-        for(let j = 1; j < limit; j++)
-        {
-            if( dataSort[i].MAKH == dataSort[j].MAKH  && flat < limit )
-                dataSort[i].fileSplit = filenum
-        }
+        
+        let n = 0
+        let key = false
+        for (let j = i + 1; j <dataSort.length; j++ )
+            if( dataSort[i].MAKH  ==  dataSort[j].MAKH )
+                n++
+
+        console.log(n,flag,limit,dataSort[i].MAKH)
 
         
-        if(flat == limit)
-            flat = 1
-        else 
-            flat++
 
+        if( parseInt(flag) + parseInt(n) < limit)
+            dataSort[i].fileSplit = filenum
+       
+        if( ( parseInt(flag) + parseInt(n) >= limit && parseInt(flag) > parseInt(n) ) || flag == limit) {
+            filenum++
+            dataSort[i].fileSplit = filenum
+            key = true
+        }
+            
+        // for(let j = 1; j < limit; j++)
+        // {
+        //     if( dataSort[i].MAKH == dataSort[j+i+1].MAKH  && flat < limit )
+        //         dataSort[i].fileSplit = filenum
+        // }
+
+        
+        if(flag != limit)
+            flag++
+        if(flag == limit) {
+            flag = 1
+            if(!key)
+                filenum++
+        }
     }
+
+    console.log(dataSort)
 
 })
